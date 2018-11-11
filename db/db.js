1ab302e0095c047ef9a39f629a3b422c8c27127c
Github <mohammadreza_t2001@yahoo.com>
2018-11-11 13:02:46 +0100

    Add to Database

--- db/db.js
+++ db/db.js
@@ -1,17 +1,48 @@
 const mysql = require('mysql');
 
-var myqlConnection = mysql.createConnection({
-  host:'localhost',
-  user:'root',
-  password:'',
-  database:'agency'
+
+const myqlConnection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'agency'
 });
 
-myqlConnection.connect((err) =>{
-  if(!err){
+myqlConnection.connect((err) => {
+  if (!err) {
     console.log('DB Connection succeded');
   } else {
     console.log('DB connection failed \n Error: ' + JSON.stringify(err, undefined, 2));
   }
 })
 
+function add(data) {
+  var sql = `INSERT INTO a_package (country, city,zip, address, events, transportation, hotel, price) VALUES ('${data.country}', '${data.city}', ${data.zip}, '${data.address}', '${data.event}', '${data.transportation}', '${data.hotel}', ${data.price})`;
+  myqlConnection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+}
+
+function get(){
+  var sql = "SELECT * FROM a_package";
+  myqlConnection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+}
+
+
+// const insertQuery = myqlConnection.connect(function (err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   var sql = "INSERT INTO a_package (country, city) VALUES ('Company Inc', 'Highway 37')";
+//   myqlConnection.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log("1 record inserted");
+//   });
+// });
+
+
+
+module.exports = add;
--- public/javascripts/package.js
+++ public/javascripts/package.js
@@ -1,2 +1,36 @@
 // Makge connection
 var socket = io.connect("http://localhost:3000/");
+
+
+$("form").submit(function (event) {
+    event.preventDefault();
+    var _country = $("#inputCountry").val();
+    var _city = $("#inputCity").val();
+    var _zip = $("#inputZip").val();
+    var _address = $("#inputAddress").val();
+    var _event = $("#inputEvent").val();
+    var _transportation = $("#inputTransportation").val();
+    var _hotel = $("#inputHotel").val();
+    var _price = $("#inputPrice").val();
+
+    socket.emit('submitPackage',{
+        country: _country,
+        city: _city,
+        zip: _zip,
+        address: _address,
+        event: _event,
+        transportation: _transportation,
+        hotel: _hotel,
+        price: _price
+    });
+   
+});
+
+socket.on('submitPackage', function(data){
+    if(data){
+        alert(data.country)
+        // alert("Data Saves to Database");
+    } else {
+        alert("Something is wrong");
+    }
+})
